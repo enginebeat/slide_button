078d4fee548cf2fe41d2c8d33763a42e48b149ac
DESKTOP-0E86V1G\luism <luis.moreira@lmtronics.co.uk>
2018-04-08 22:48:55 +0100

    First commit. Not really just one slidding button but rather the full Rasp pi outputs

---
+++ index.js
@@ -0,0 +1,107 @@
+var switch3= document.getElementById('myonoffswitch3');
+var switch5= document.getElementById('myonoffswitch5');
+var switch7= document.getElementById('myonoffswitch7');
+var switch8= document.getElementById('myonoffswitch8');
+var switch10= document.getElementById('myonoffswitch10');
+var switch11= document.getElementById('myonoffswitch11');
+var switch12= document.getElementById('myonoffswitch12');
+var switch13= document.getElementById('myonoffswitch13');
+var switch15= document.getElementById('myonoffswitch15');
+var switch16= document.getElementById('myonoffswitch16');
+var switch18= document.getElementById('myonoffswitch18');
+var switch19= document.getElementById('myonoffswitch19');
+var switch21= document.getElementById('myonoffswitch21');
+var switch22= document.getElementById('myonoffswitch22');
+var switch23= document.getElementById('myonoffswitch23');
+var switch24= document.getElementById('myonoffswitch24');
+var switch26= document.getElementById('myonoffswitch26');
+var switch29= document.getElementById('myonoffswitch29');
+var switch31= document.getElementById('myonoffswitch31');
+var switch32= document.getElementById('myonoffswitch32');
+var switch33= document.getElementById('myonoffswitch33');
+var switch35= document.getElementById('myonoffswitch35');
+var switch36= document.getElementById('myonoffswitch36');
+var switch37= document.getElementById('myonoffswitch37');
+var switch38= document.getElementById('myonoffswitch38');
+var switch40= document.getElementById('myonoffswitch40');
+
+
+switch3.addEventListener('click', ()=>{
+
+});
+switch5.addEventListener('click', ()=>{
+
+});
+switch7.addEventListener('click', ()=>{
+
+});
+switch8.addEventListener('click', ()=>{
+
+});
+switch10.addEventListener('click', ()=>{
+
+});
+switch11.addEventListener('click', ()=>{
+
+});
+switch12.addEventListener('click', ()=>{
+
+});
+switch13.addEventListener('click', ()=>{
+
+});
+switch15.addEventListener('click', ()=>{
+
+});
+switch16.addEventListener('click', ()=>{
+
+});
+switch18.addEventListener('click', ()=>{
+
+});
+switch19.addEventListener('click', ()=>{
+
+});
+switch21.addEventListener('click', ()=>{
+
+});
+switch22.addEventListener('click', ()=>{
+
+});
+switch23.addEventListener('click', ()=>{
+
+});
+switch24.addEventListener('click', ()=>{
+
+});
+switch26.addEventListener('click', ()=>{
+
+});
+switch29.addEventListener('click', ()=>{
+
+});
+switch31.addEventListener('click', ()=>{
+
+});
+switch32.addEventListener('click', ()=>{
+
+});
+switch33.addEventListener('click', ()=>{
+
+});
+switch35.addEventListener('click', ()=>{
+
+});
+switch36.addEventListener('click', ()=>{
+
+});
+switch37.addEventListener('click', ()=>{
+
+});
+switch38.addEventListener('click', ()=>{
+
+});
+switch40.addEventListener('click', ()=>{
+
+});
+
