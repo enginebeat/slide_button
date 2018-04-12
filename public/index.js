29d44e864cd25a3b000e737b0ccc6ff2c9fb4f19
enginebeat <luis.moreira@lmtronics.co.uk>
2018-04-12 23:26:27 +0000

    added feedback from server on output status when the client connects to it

--- public/index.js
+++ public/index.js
@@ -7,6 +7,11 @@ console.log(serverIPAddress);
 console.log(serverPort);
 var socket = io.connect('http://' + serverIPAddress + ':' + serverPort);
 
+/* adds the global event handler 
+it will return the class of the owner of the event, in
+this case any of the switches. logic needs to be added 
+to check if it is a switch thart got changed.
+*/
 function addGlobalEventHandler(){
     document.body.addEventListener("change", (event) => {        
         switchChanged(event.target.className);
@@ -15,7 +20,7 @@ function addGlobalEventHandler(){
 
 window.onload = initPage;
 
-
+/* called when any of the switches is toggled */
 function switchChanged(cName){
     searchPosition = cName.search('-');
     let switchName = cName.slice(0,searchPosition);
@@ -41,6 +46,13 @@ function initPage(){
 
 socket.on('GPIOStatus', (data)=>{
     console.log(data);
+    for (var gpioN in data){
+        let switchN = `onoffswitch${gpioN.slice(6)}`;
+        let state = data[gpioN];
+        console.log(switchN);
+        console.log(state);
+        document.getElementById('my' + switchN).checked = state;   
+    };
 });
 
 socket.on('disconnect', ()=>{
