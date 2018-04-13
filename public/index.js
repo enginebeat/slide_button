c1779d9d62e08b1f0ebf9ae92f524c373bceac1a
enginebeat <luis.moreira@lmtronics.co.uk>
2018-04-13 02:57:19 +0000

    Added code for setting and clearing all pins but I'm not happy with the place and also with the GPIOStatus socket return code

--- public/index.js
+++ public/index.js
@@ -44,6 +44,7 @@ function initPage(){
     
 }
 
+/* reply from server with the status of the GPIO */
 socket.on('GPIOStatus', (data)=>{
     console.log(data);
     for (var gpioN in data){
@@ -53,6 +54,20 @@ socket.on('GPIOStatus', (data)=>{
         console.log(state);
         document.getElementById('my' + switchN).checked = state;   
     };
+
+    document.getElementById('clear_all_btn').addEventListener('click', ()=>{
+        console.log("Clear All");
+
+        //Probably shouldn't be here
+    });
+    
+    document.getElementById('set_all_btn').addEventListener('click', ()=>{
+        console.log('Set All');
+
+        //Probably shouldn't be here
+
+    });
+
 });
 
 socket.on('disconnect', ()=>{
@@ -72,3 +87,49 @@ socket.on('connect', ()=>{
     //document.getElementById('s_connection').innerHTML = 'Connected To The Server!';
     requestGPIOStatus();
 });
+
+
+var setBtn = document.addEventListener
+
+
+/* Code to make all the content draggable */
+dragElement(document.getElementById(("content")));
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id)) {
+      /* if present, the header is where you move the DIV from:*/
+      document.getElementById(elmnt.id).onmousedown = dragMouseDown;
+    } else {
+      /* otherwise, move the DIV from anywhere inside the DIV:*/
+      elmnt.onmousedown = dragMouseDown;
+    }
+  
+    function dragMouseDown(e) {
+      e = e || window.event;
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+  
+    function elementDrag(e) {
+      e = e || window.event;
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+  
+    function closeDragElement() {
+      /* stop moving when mouse button is released:*/
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
